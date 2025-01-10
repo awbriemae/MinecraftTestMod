495a5eee95963382650fedb69bd4e537e63b386b
TaKawwa <mikolaj.dudek@onet.pl>
2025-01-10 19:54:49 +0000

    Added 2 items. Mulch and Kebab. Only images I had available near me at the time.

--- src/main/java/com/aubrey/tutorial/MallowWonderland.java
+++ src/main/java/com/aubrey/tutorial/MallowWonderland.java
@@ -1,6 +1,8 @@
 package com.aubrey.tutorial;
 
+import com.aubrey.tutorial.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -16,8 +18,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod
+@Mod(MallowWonderland.MOD_ID)
+public class MallowWonderland
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "mallowwonderland";
@@ -25,7 +27,7 @@ public class TutorialMod
     public static final Logger LOGGER = LogUtils.getLogger();
 
 
-    public TutorialMod()
+    public MallowWonderland()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -34,6 +36,8 @@ public class TutorialMod
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -49,6 +53,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.MULCH);
+            event.accept(ModItems.KEBAB);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/aubrey/tutorial/item/ModItems.java
@@ -0,0 +1,26 @@
+package com.aubrey.tutorial.item;
+
+import com.aubrey.tutorial.MallowWonderland;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    // DeferredRegister<"type"> - This tells Minecraft what we want to register
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MallowWonderland.MOD_ID);
+
+    public static final RegistryObject<Item> MULCH = ITEMS.register("mulch",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> KEBAB = ITEMS.register("kebab",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+
+}
